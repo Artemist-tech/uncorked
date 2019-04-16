1594fee0cb929f2451b34a6140dc444c09f89b76
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2019-04-16 13:41:00 +1000

    tactile descriptors added

--- sample-data/data/character.js
+++ sample-data/data/character.js
@@ -144,3 +144,5 @@ const Characteristics = {
     ]
   }
 };
+
+export default Characteristics;
--- sample-data/models/Tactile-Descriptors.js
+++ sample-data/models/Tactile-Descriptors.js
@@ -4,18 +4,10 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const TactileSchema = new Schema({
-  acidity: {
-    type: String
-  },
-  body: {
-    type: String
-  },
-  tannin: {
-    type: String
-  },
-  finish: {
-    type: String
-  }
+  acidity: String,
+  body: String,
+  tannin: String,
+  finish: String
 });
 
 const TactileDescriptors = mongoose.model('tactile-descriptors', TactileSchema);
