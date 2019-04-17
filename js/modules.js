8f5b1b7106a71f113b085ab08e1c70632f1be6af
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2019-04-18 09:21:58 +1000

    genreral housekeeping

--- js/modules.js
+++ js/modules.js
@@ -7,6 +7,7 @@ exports.modules = [
   './js/dist/scrolling-navbar.js',
   './js/vendor/waves.js',
   './js/dist/forms-free.js',
-  './js/vendor/enhanced-modals.js'
+  './js/vendor/enhanced-modals.js',
+  './js/dist/default-file-input.js',
   // './js/vendor/addons/datatables.js'
 ];
