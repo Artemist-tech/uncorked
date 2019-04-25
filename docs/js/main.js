5e08289ecef6de5c7cfc2cb1201b08d3812b676d
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2019-04-26 08:51:10 +1000

    updated docs folder

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,7 @@
 const redWines = document.querySelector('#grape-varietal-red');
 const whiteWines = document.querySelector('#grape-varietal-white');
+const sparklingWines = document.querySelector('#grape-varietal-sparkling');
+const roseWines = document.querySelector('#grape-varietal-rose');
 
 $('.wine-button').on('click', showGrapes);
 
@@ -9,10 +11,26 @@ function showGrapes(e) {
     case 'grape-white':
       whiteWines.classList.remove('invisible');
       redWines.classList.add('invisible');
+      sparklingWines.classList.add('invisible');
+      roseWines.classList.add('invisible');
       break;
     case 'grape-red':
-      whiteWines.classList.add('invisible');
       redWines.classList.remove('invisible');
+      whiteWines.classList.add('invisible');
+      sparklingWines.classList.add('invisible');
+      roseWines.classList.add('invisible');
+      break;
+    case 'grape-sparkling':
+      sparklingWines.classList.remove('invisible');
+      whiteWines.classList.add('invisible');
+      redWines.classList.add('invisible');
+      roseWines.classList.add('invisible');
+      break;
+    case 'grape-rose':
+      roseWines.classList.remove('invisible');
+      sparklingWines.classList.add('invisible');
+      whiteWines.classList.add('invisible');
+      redWines.classList.add('invisible');
       break;
     default:
       break;
@@ -21,13 +39,21 @@ function showGrapes(e) {
 
 $('.white-wine-varietal').on('click', toggleClicked);
 $('.red-wine-varietal').on('click', toggleClicked);
+$('.sparkling-wine-varietal').on('click', toggleClicked);
+$('.rose-wine-varietal').on('click', toggleClicked);
 
 function toggleClicked(e) {
   const grapeClicked = e.target;
-  if (grapeClicked.classList.contains('white-wine-varietal')) {
+  if (
+    grapeClicked.classList.contains('white-wine-varietal') ||
+    grapeClicked.classList.contains('sparkling-wine-varietal')
+  ) {
     grapeClicked.classList.toggle('btn-outline-warning');
     grapeClicked.classList.toggle('btn-warning');
-  } else {
+  } else if (
+    grapeClicked.classList.contains('red-wine-varietal') ||
+    grapeClicked.classList.contains('rose-wine-varietal')
+  ) {
     grapeClicked.classList.toggle('btn-outline-danger');
     grapeClicked.classList.toggle('btn-danger');
   }
