84c31ef71e4f544c4795ca00672858c0d01bb6fe
PaulHaze <20569604+PaulHaze@users.noreply.github.com>
2019-04-30 15:53:36 +1000

    final reds added

---
+++ sample-data/data/varietals/red/malbec.js
@@ -0,0 +1,22 @@
+const malbec = {
+  varietal: 'Malbec',
+  botanical: [
+    'Blueberry',
+    'Blackberry',
+    'Boysenberry',
+    'Black cherry',
+    'Sugar Plum',
+    'Grape jam',
+    'Red plum',
+    'Raspberry',
+    'Prune',
+    'Raisin',
+    'Sage',
+    'Violets'
+  ],
+  oakAge: ['Espresso', 'Cocoa powder', 'Mocha', 'Chocolate', 'Vanilla', 'Tobacco'],
+  savoury: ['Red pepper flakes', 'Licorice', 'Dried leaves', 'Spicy'],
+  allOther: ['Clay pot', 'Tar', 'Graphite', 'Smoke']
+};
+
+export { malbec };
---
+++ sample-data/data/varietals/red/shiraz-syrah.js
@@ -0,0 +1,46 @@
+const shirazSyrah = {
+  varietal: 'Shiraz / Syrah',
+  botanical: [
+    'Black cherry',
+    'Blackberry',
+    'Blackcurrant',
+    'Blueberry',
+    'Eucalyptus',
+    'Herbs',
+    'Mint',
+    'Mulberry',
+    'Plum',
+    'Raisin',
+    'Raspberry',
+    'Redcurrant',
+    'Stewed plums',
+    'Boysenberry',
+    'Bramble fruits',
+    'Jam',
+    'Red cherry',
+    'Dried cranberry',
+    'Sage',
+    'Dried herbs'
+  ],
+  oakAge: ['Coffee', 'Cigar box', 'Chocolate', 'Cedar', 'Coconut', 'Toast', 'Vanilla', 'Camphor'],
+  savoury: [
+    'Black olive',
+    'Star anise',
+    'Black pepper',
+    'Green peppercorn',
+    'Licquorice',
+    'Bacon fat',
+    'Cured meat',
+    'Black cardamom',
+    'Aniseed',
+    'Nutmeg',
+    'Spicy',
+    'Mushroom',
+    'Salami',
+    'Soy',
+    'Menthol'
+  ],
+  allOther: ['Tar', 'Graphite', 'Leather', 'Sassafras']
+};
+
+export { shirazSyrah };
